80a83bf51f57eb78421e4d9951d6bd0cfeebf443
Vlatko <vlatce.vlatko@hotmail.com>
2013-06-05 16:22:38 +0200

    isDead() in Agent, main improved;

--- SuperEmo/SuperEmo/Program.cs
+++ SuperEmo/SuperEmo/Program.cs
@@ -9,56 +9,60 @@ namespace SuperEmo
     {
         static void Main(string[] args)
         {
-            Enviroment env = new Enviroment();
-            Agent nevena = new Agent();
-            nevena.sensitivity = 1;
-            nevena.curiosity = 0;
-       //     nevena.load("nevena.txt");
+            //for (int j = 0; j < 50; j++)
+            //{
+                Enviroment env = new Enviroment();
+                Agent nevena = new Agent();
+                nevena.sensitivity = 1;
+                nevena.curiosity = 0;
+                nevena.load("nevena.txt");
 
-            //PROMENA
-            for (int i = 0; i < 300; i++)
-            {
-                Console.Clear();
-                env.drawEnvironment();
-                Console.WriteLine(nevena.getState());
+                //PROMENA
+                for (int i = 0; i < 300; i++)
+                {
+                    //Console.Clear();
+                    env.drawEnvironment();
+                    int a = nevena.getAction(); //zemi akcija od agent
+                    Console.WriteLine("State: " + nevena.getState() + " I will do: " +a+"\n");
+                    System.Threading.Thread.Sleep(100);
 
-                int[] tiles = env.lastNStates(4); //zadnji 4
-                int a = nevena.getAction(); //zemi akcija od agent
-                //ako smo poginuli
-                if(a==-99){
-                    Console.WriteLine("nevena je poginula");
-                    nevena.lives++;
-                    break;
+                    int[] tiles = env.lastNStates(4); //zadnji 4
+                    //ako smo poginuli
+                    if (nevena.isDead(nevena.getState(), tiles[0]))
+                    {
+                        Console.WriteLine("Nevena je poginula :S");
+                        nevena.lives++;
+                        break;
+                    }
+                    //ako smo preziveli
+                    nevena.takeAction(a, a, tiles[0], tiles[1], tiles[2]); //na agent take action so akcijata od prethodniot cekor
+                    env.generateState();
                 }
 
-
-                nevena.takeAction(a,a,tiles[0],tiles[1],tiles[2]); //na agent take action so akcijata od prethodniot cekor
-                env.generateState();
-            }
-
-            //verify
-            int totalIncrements = 0;
-            for (int i1 = 0; i1 < 3; i1++)
-            {
-                for (int i2 = 0; i2 < 3; i2++)
+                //verify
+                int totalIncrements = 0;
+                for (int i1 = 0; i1 < 3; i1++)
                 {
-                    for (int i3 = 0; i3 < 5; i3++)
+                    for (int i2 = 0; i2 < 3; i2++)
                     {
-                        for (int i4 = 0; i4 < 5; i4++)
+                        for (int i3 = 0; i3 < 5; i3++)
                         {
-                            for (int i5 = 0; i5 < 5; i5++)
+                            for (int i4 = 0; i4 < 5; i4++)
                             {
-                                totalIncrements += nevena.genome[i1, i2, i3, i4, i5] ;
+                                for (int i5 = 0; i5 < 5; i5++)
+                                {
+                                    totalIncrements += nevena.genome[i1, i2, i3, i4, i5];
+                                }
                             }
                         }
                     }
                 }
+
+                Console.WriteLine("total " + totalIncrements + "  coins " + nevena.gold);
+
+                nevena.save("nevena.txt");
+                Console.Write("\n\n\n");
             }
-
-            Console.WriteLine("total " + totalIncrements + "  coins " + nevena.gold);
-
-            nevena.save("nevena.txt");
-            Console.Write("\n");
-        }
+        //}
     }
 }
